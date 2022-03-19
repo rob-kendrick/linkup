384aa0232dc08d1b16e2e871bd9fddc307d510b4
rob-kendrick <kendrickrobert99@gmail.com>
2022-03-19 13:14:08 +0100

    feat(server): added userController boilerplate, added user routes in router.ts

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,5 +1,68 @@
 // Below is some boilerplate data which will be changed once DB and models are there
-
+// LEGEND :
+// 🚀🚀🚀 = New section
+// ✅ = To be included in MVP
+// 🅱️ = Expect request body
+// 🅿️ = Expect Params (eg. Id)
+// --------------------------------------------------------
 // const UserModel = require('../models/modelName1')
+// --------------------------------------------------------
+// 🚀🚀🚀 USER CONTROLLERS 🚀🚀🚀
 
-// module.exports = { fn1, fn2, fn3, etc... };
+// Create 1 user 🅱️ ✅
+async function createUser(req, res) {
+  try {
+
+  } catch (err) {
+
+  }
+}
+
+// Get 1 user by ID 🅿️ ✅
+async function getUserById(req, res) {
+  try {
+
+  } catch (err) {
+
+  }
+}
+
+// Get all users
+async function getAllUsers(req, res) {
+  try {
+
+  } catch (err) {
+
+  }
+}
+
+// Edit 1 user by ID 🅱️ 🅿️
+async function editUser(req, res) {
+  try {
+
+  } catch (err) {
+
+  }
+}
+
+// Delete 1 user by ID 🅿️
+async function deleteUser(req, res) {
+  try {
+
+  } catch (err) {
+
+  }
+}
+
+// Add friend 🅿️
+// async function to be added !
+
+// Remove friend 🅿️
+// async function to be added !
+
+// --------------------------------------------------------
+// 🚀🚀🚀 EXPORTS 🚀🚀🚀
+// module.exports = {
+//   createUser, getUserById, getAllUsers, editUser, deleteUser,
+// };
+// --------------------------------------------------------
--- server/router.ts
+++ server/router.ts
@@ -2,11 +2,11 @@
 // 🚀🚀🚀 = New section
 // ✅ = To be included in MVP
 // 🅱️ = Expect request body
-
+// 🅿️ = Expect Params (eg. Id)
 // --------------------------------------------------------
 // IMPORTS
 const { Router } = require('express');
-// const userController = require('./controllers/userController.ts');
+const userController = require('./controllers/userController.ts');
 const eventController = require('./controllers/eventController.ts');
 
 const router = Router();
@@ -18,40 +18,47 @@ const router = Router();
 // Get all events ✅
 router.get('/events', eventController.getAllEvents);
 
-// Get 1 event ✅
+// Get 1 event by ID 🅿️ ✅
 router.get('events/:eventid', eventController.getEventById);
 
 // Create 1 event 🅱️ ✅
 router.post('/events', eventController.createEvent);
 
-// Join 1 event ✅
+// Join 1 event 🅿️ ✅
 router.patch('/events/join/:eventid/:userid', eventController.joinEvent);
 
-// Leave 1 event ✅
+// Leave 1 event 🅿️ ✅
 router.patch('/events/leave/:eventid/:userid', eventController.leaveEvent);
 
-// Edit 1 event 🅱️
+// Edit 1 event by ID 🅿️ 🅱️
 router.patch('/events/:eventid', eventController.editEvent);
 
-// Delete 1 event
+// Delete 1 event 🅿️
 router.delete('/events/:eventid', eventController.deleteEventById);
 
 // --------------------------------------------------------
 // 🚀🚀🚀 USER ROUTES 🚀🚀🚀
 
 // Create 1 user 🅱️ ✅
+router.post('/users', userController.createUser);
 
-// Get 1 user by ID ✅
+// Get 1 user by ID 🅿️ ✅
+router.get('/users/:userid', userController.getUserById);
 
 // Get all users
+router.get('/users', userController.getAllUsers);
 
-// Edit 1 user 🅱️
+// Edit 1 user by ID 🅿️ 🅱️
+router.patch('/users/:userid', userController.editUser);
 
-// Delete 1 user
+// Delete 1 user by ID 🅿️
+router.delete('/users/:userid', userController.deleteUser);
 
-// Add friend
+// Add friend 🅿️
+// route and controller Func to be added !
 
-// Remove friend
+// Remove friend 🅿️
+// route and controller func to be added !
 
 // --------------------------------------------------------
 // 🚀🚀🚀 LOGIN + LOGOUT ROUTES 🚀🚀🚀
