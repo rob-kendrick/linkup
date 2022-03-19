67a42ceb2dd0bd4082c114f1aef3a38e62c882fe
rob-kendrick <kendrickrobert99@gmail.com>
2022-03-19 11:33:59 +0100

    feat(server): setup dev dependencies, added readme, cleaned up index.ts

---
+++ server/README.md
@@ -0,0 +1,6 @@
+# Server Info
+
+- make sure to have ESLint installed and set as default formatter
+- run `npm i` inside the server folder before starting the server.
+- to run the server with nodemon, do `npx nodemon ./index.ts`
+- To manually compile the TS files into JS, do `npm run build`. This will transpile the server into JS. This should not be done during development, but at the end, when we are ready to deploy.
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+import morgan from 'morgan'; // eslint-disable-line import/no-extraneous-dependencies
+import cors from 'cors';
+// const router = require('./router.ts')
+const PORT = 4000;
 
 const app = express();
 
+app.use(cors());
+app.use(morgan('short'));
+app.use(express.json());
+// app.use(router)
+
 app.get('/', (req, res) => {
-  res.send('😇😇😇😇😇');
+  res.send('hi hi😇😇😇😇😇');
 });
 
-app.listen(4000, () => {
-  console.log('🚀😘🚀The application is listening on port localhost 4000!🚀🚀🚀');
+app.listen(PORT, () => {
+  console.log(`🚀🚀🚀 Server up and listening on http://localhost:${PORT}!🚀🚀🚀`); // eslint-disable-line
 });
