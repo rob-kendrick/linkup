885e877797b8452a7c72f4c00ece55fc10c02779
lukasz <lukasz.tylke@gmail.com>
2022-03-24 17:01:51 +0100

    feat: setup

--- client/src/components/HeaderReturn/HeaderReturn.tsx
+++ client/src/components/HeaderReturn/HeaderReturn.tsx
@@ -5,11 +5,9 @@ function HeaderReturn() {
   const navigate = useNavigate();
   return (
     <div>
-      <h3>HeaderReturn</h3>
-      <div className="space-around-delete">
-        <button type="button" onClick={() => navigate(-1)}>Go Back</button>
-        <div>Reset/Edit Button</div>
-      </div>
+      <div>Arrow</div>
+      <div>Text</div>
+      <div>Reset</div>
     </div>
   );
 }
