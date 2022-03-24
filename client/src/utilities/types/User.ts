d8dc88aa22a2cb56a900bc1678648c00904b6c7e
Dansando8 <d.sandoval@vertciel.ca>
2022-03-24 18:04:50 +0100

    refactor(merging components to dev): dev branch has been pulled to integrate map-component files
    Cleaned files and prepared branch for modifying files so merge is clean according to the app routing
    
    BREAKING CHANGE: Many files modified on the map-component

--- client/src/utilities/types/User.ts
+++ client/src/utilities/types/User.ts
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-// import type { LU_Event } from './LU_Event';
-// import type { Rating } from './Rating';
-
-export interface User {
-  user_id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  password: string;
-  bio: string;
-  profile_picture: string;
-  events_created: string[];
-  events_participating: string[];
-  friends: string[];
-  rating: string[];
-=======
 import type { EventBasicData } from './EventBasicData';
 
 export interface User {
@@ -28,5 +11,4 @@ export interface User {
   events_participating: EventBasicData[]
   password: string;
   date_created: Date;
->>>>>>> dev
 }
