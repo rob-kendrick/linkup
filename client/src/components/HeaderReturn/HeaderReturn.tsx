a987b72cc2130261a26b0e696dc972dcb897df12
lukasz <lukasz.tylke@gmail.com>
2022-03-24 20:25:58 +0100

    style(HeaderReturn): improve component styling

--- client/src/components/HeaderReturn/HeaderReturn.tsx
+++ client/src/components/HeaderReturn/HeaderReturn.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import SvgArrow from '../../assets/IoIosArrowBack.svg';
+import { ReactComponent as SvgArrow } from '../../assets/IoIosArrowBack.svg';
+import './headerReturn.css';
 
-function HeaderReturn() {
+type HeaderReturnProps = {
+  option?: boolean
+};
+
+function HeaderReturn(props: HeaderReturnProps) {
   const navigate = useNavigate();
+  // const { option, text } = props;
+  const option = true;
+  const text = 'Create activity';
   return (
-    <div>
-      <button type="button" onClick={() => navigate(-1)}>
-        <SvgArrow />
-      </button>
-      <div>Text</div>
-      <div>Reset</div>
+    <div className="headerReturn">
+      <div className="headerReturn__container">
+        <button className="headerReturn__btn" type="button" onClick={() => navigate(-1)}>
+          <SvgArrow />
+        </button>
+        <div className="headerReturn__txt">{text}</div>
+      </div>
+      {option ? <div className="headerReturn__option--visible"><p>Reset</p></div>
+        : <div className="headerReturn__option--hidden" />}
     </div>
   );
 }
