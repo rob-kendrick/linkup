9c2cba4e211beec6aefb5ace23381ef03e0e8213
lukasz <lukasz.tylke@gmail.com>
2022-03-24 17:28:35 +0100

    feat(HeaderReturn): set up svg

--- client/src/components/HeaderReturn/HeaderReturn.tsx
+++ client/src/components/HeaderReturn/HeaderReturn.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import SvgArrow from '../../assets/IoIosArrowBack.svg';
 
 function HeaderReturn() {
   const navigate = useNavigate();
   return (
     <div>
-      <div>Arrow</div>
+      <button type="button" onClick={() => navigate(-1)}>
+        <SvgArrow />
+      </button>
       <div>Text</div>
       <div>Reset</div>
     </div>
