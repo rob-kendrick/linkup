98915903c709d5e41b9068806dc6e875d007cd9b
Peter hp <phirschparker@gmail.com>
2022-03-25 18:19:42 +0100

    feat(menulistitem): create menulist item with styling
    BREAKING CHANGE: added menuListitem that requires props passed to it - currently nothing is being
    passed

--- client/src/components/MenuList/MenuListItem/MenuListItem.tsx
+++ client/src/components/MenuList/MenuListItem/MenuListItem.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import './MenuListItem.css';
+import { Link } from 'react-router-dom';
 
-function MenuListItem() {
+export interface MenuListItemProps {
+    link: string;
+    text: string;
+    svgLogo: string;
+}
+
+function MenuListItem({ link, text, svgLogo }:MenuListItemProps) {
   return (
-    <div>MenuListItem</div>
+    <Link to={link}>
+      <div className="MenuListItem">
+        <img className="mli_icon" src={svgLogo} alt={text} />
+        <p className="mli_text">{text}</p>
+        <span className="mli-rightarrow">&#5171;</span>
+      </div>
+      <hr />
+
+    </Link>
+
   );
 }
 
