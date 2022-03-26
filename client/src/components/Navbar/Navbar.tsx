2b6eb1aeac5ed328250a93956ed8fb283bef401e
Peter hp <phirschparker@gmail.com>
2022-03-26 08:41:18 +0100

    feat(navbar): merged, on click style change working
    BREAKING CHANGE: can not see first icon

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-=======
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
->>>>>>> 4233f27b3c06cc6211fbe1bfb0c483f6b35587c7
 import { ReactComponent as HiSearch } from '../../assets/HiSearch.svg';
 import { ReactComponent as BiCalendarHeart } from '../../assets/BiCalendarHeart.svg';
 import { ReactComponent as HiChat } from '../../assets/HiChat.svg';
@@ -26,22 +21,9 @@ function Navbar() {
           onClick={() => handleClick(1)}
         >
           <div className="n__icon-container">
-            {/* <HiSearch id="clip" className="n__icon" /> */}
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 20 20"
-              height="2em"
-              width="2em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <clipPath id="clip">
-                <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
-              </clipPath>
-            </svg>
+            <HiSearch className="n__icon" />
           </div>
-          <div>Browse</div>
+          <div>My Activities</div>
         </button>
       </Link>
 
