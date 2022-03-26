21a548a445b44e87f0d9673aaac0f323451895e2
Dansando8 <d.sandoval@vertciel.ca>
2022-03-26 13:05:41 +0100

    style(eventcard & tagslist components): added CSS

--- client/src/components/EventCard/EventCard.tsx
+++ client/src/components/EventCard/EventCard.tsx
@@ -20,36 +20,50 @@ function EventCard({ event, eventList }: Events) {
       key={event.id_event}
     >
 
-      <div key={event.id_event} className="activity-card">
-        <div>
-          {event.tags}
+      <div key={event.id_event} className="ac_main-container">
+        <div className="tl_component-container">
+          <TagList tags={event.tags} />
         </div>
-        <div className="activity-card-profile">
-          <div>
-            <div><img src={event.creator.profile_picture} alt="event creator" className="activity-card-picture" /></div>
+        <div className="ac_profile">
+          <div className="ac_profile-inner-container">
+            <div className="ac_profile-picture-container">
+              <div><img src={event.creator.profile_picture} alt="event creator" className="ac_profile-picture" /></div>
+            </div>
+            <div className="ac_header-info">
+              <h4>{event.title}</h4>
+              <p>{event.creator.first_name}</p>
+            </div>
           </div>
-          <div className="activity-card-header-info">
-            <h4 id="activity-card-title">{event.title}</h4>
-            <p id="activity-card-user">{event.creator.first_name}</p>
+        </div>
+        <div className="ac_details">
+          <div className="ac_details-inner-container">
+            <div className="ac_icon-container">
+              <div className="ac_details-icon"><img src={marker} alt="marker pin" /></div>
+            </div>
+            <div className="ac_details-text">
+              <p>
+                {event.street_name}
+                {' '}
+                {event.street_number}
+                {', '}
+                {event.postcode}
+                {' '}
+                {event.city}
+              </p>
+            </div>
           </div>
         </div>
-        <div className="activity-card-details">
-          <div className="activity-card-details-icon"><img src={marker} alt="marker pin" /></div>
-          <p>
-            {event.street_name}
-            {' '}
-            {event.street_number}
-            {', '}
-            {event.postcode}
-            {' '}
-            {event.city}
-          </p>
+        <div className="ac_details">
+          <div className="ac_details-inner-container">
+            <div className="ac_icon-container">
+              <div className="ac_details-icon"><img src={marker} alt="marker pin" /></div>
+            </div>
+            <div className="ac_details-text">
+              <p>{moment(event.date).format('h:mm')}</p>
+            </div>
+          </div>
         </div>
-        <div className="activity-card-details">
-          <div className="activity-card-details-icon"><img src={marker} alt="marker pin" /></div>
-          <p>{moment(event.date).format('h:mm')}</p>
-        </div>
-        <div className="activity-card-button-container">
+        <div className="ac_button-container">
           <ButtonEventsMenu name="LinkUp" />
           <ButtonEventsMenu name={`${event.participants.length} participants`} />
         </div>
