6bbb56732159ce150223f69a87c6ddc85efa3101
lthemis <lukasz.tylke@gmail.com>
2022-03-29 13:33:05 +0200

    style(chatgroup): add basic styling

--- client/src/views/Chat/ChatGroup/ChatGroup.tsx
+++ client/src/views/Chat/ChatGroup/ChatGroup.tsx
@@ -11,14 +11,24 @@ interface LocationState {
 export default function ChatGroup() {
   const location = useLocation();
   const { state } = location as LocationState;
-  console.log(state);
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    console.log('chat msg');
+  };
 
   return (
     <article className="cG">
       <HeaderReturn text={state.currentEvent.title} luEvent={state.currentEvent} />
       <div className="cG__mainContainer">
         <div className="cG__chatMsgsContainer">Chat</div>
-        <div className="cG__inputContainer">Input</div>
+        <div className="cG__inputContainer">
+          <form onSubmit={(e) => submitHandler(e)}>
+            <label htmlFor="chat" />
+            <input id="chat" name="chat" type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
     </article>
   );
