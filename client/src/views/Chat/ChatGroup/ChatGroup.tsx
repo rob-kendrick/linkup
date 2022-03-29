fe8e1d5c4ae12e38c9e9c20bd58f73422808c013
lthemis <lukasz.tylke@gmail.com>
2022-03-29 18:36:20 +0200

    feat(client): initial communication between client and server using socket.io

--- client/src/views/Chat/ChatGroup/ChatGroup.tsx
+++ client/src/views/Chat/ChatGroup/ChatGroup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { io, Socket } from 'socket.io-client';
 import HeaderReturn from '../../../components/HeaderReturn/HeaderReturn';
 import { LuEvent } from '../../../utilities/types/Event';
 import './chatGroup.css';
@@ -8,13 +9,31 @@ interface LocationState {
   state: { currentEvent: LuEvent }
 }
 
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  emitMsgFromClient: (userId: number, eventId: number, msg: string) => void;
+}
+
 export default function ChatGroup() {
+  const userId = localStorage.getItem('ud_user');
   const location = useLocation();
   const { state } = location as LocationState;
+  const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:4000');
+  socket.on('basicEmit', (sth: any) => {
+    console.log(sth);
+  });
 
   const submitHandler = (e: any) => {
     e.preventDefault();
+    const msg = e.target.elements.chat.value;
+
     console.log('chat msg');
+    socket.emit('emitMsgFromClient', Number(userId), state.currentEvent.id_event, msg);
   };
 
   return (
