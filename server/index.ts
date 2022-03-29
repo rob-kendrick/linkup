e54b41e687cf55e26b135189e264d215eebeec4a
lthemis <lukasz.tylke@gmail.com>
2022-03-29 18:36:55 +0200

    feat(server): initial communication between client and server using socket IO

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,12 @@ import express from 'express';
 import morgan from 'morgan'; // eslint-disable-line import/no-extraneous-dependencies
 import cors from 'cors';
 // eslint-disable-next-line
+import { Server } from "socket.io";
+import { createServer } from 'http';
 import router from './routes/index';
 // import prisma from './db';
+const http = require('http');
+const socketio = require('socket.io');
 
 const corsConfig = {
   // REMOVE-START
@@ -17,11 +21,41 @@ const PORT = 4000;
 
 const app = express();
 
+// io.on('connection', (socket:any) => {
+// //   console.log('SOCKET IO!');
+// //   // socket.on('hello', () => {
+// //   //   // ...
+// //   // });
+// //   // socket.emit('user connected');
+// });
+
+interface ServerToClientEvents {
+  basicEmit: (msg: string) => void;
+}
+interface ClientToServerEvents {
+  emitMsgFromClient: (userId: number, eventId: number, msg: string) => void;
+}
+
 app.use(cors());
 app.use(morgan('short'));
 app.use(express.json());
 app.use('/', router);
+const httpServer = createServer(app);
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('SOCKET');
+  socket.emit('basicEmit', 'msg1');
+  socket.on('emitMsgFromClient', (userId, eventId, msg) => {
+    console.log(userId, eventId, msg);
+  });
+});
+
+httpServer.listen(PORT, () => {
   console.log(`🚀🚀🚀 Server up and listening on http://localhost:${PORT} ! 🚀🚀🚀`); // eslint-disable-line
 });
