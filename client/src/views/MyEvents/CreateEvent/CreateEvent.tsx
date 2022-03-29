8e3f6953b9755d425bba7163be6be7f7bab94c07
rob-kendrick <kendrickrobert99@gmail.com>
2022-03-29 11:25:55 +0200

    feat(create event): refactored some notification stuff for create event, added a timeout redirect
    added a timeout redirect on server response success

--- client/src/views/MyEvents/CreateEvent/CreateEvent.tsx
+++ client/src/views/MyEvents/CreateEvent/CreateEvent.tsx
@@ -8,6 +8,7 @@ import { LuEvent } from '../../../utilities/types/Event';
 import eventApi from '../../../utilities/api/event.api';
 import './CreateEvent.css';
 import UserList from '../../../components/SelectUsers/UserList/UserList';
+import MapSmall from '../../../components/MapSmall/MapSmall';
 
 // TODO: delete mockAddress when retrieve lat, lng and adress from map
 const mockAddress = {
@@ -53,17 +54,20 @@ function CreateEvent() {
     const user = Number(localStorage.getItem('id_user'));
     if (user) {
       try {
+        // creating the request body
         const fullEvent = Object.assign(formData, mockAddress);
         fullEvent.participants_to_add = participantsToAdd;
         fullEvent.creator_id = user;
+        // Posting the event
         const response = await eventApi.postEvent(fullEvent);
+        // If event creation is sucess, redirect to 'my events'
         if (response.data) {
-          console.log('success !!!!!!!!');
-          navigate('/myevents');
+          setTimeout(() => { navigate('/myevents'); }, 2000);
         }
         console.log('AAAAAAAAAAH', response);
+        // Error handling
         if (response.error) setNotification('Server error');
-        else setNotification('Event created!');
+        else setNotification('Event successfully created!');
       } catch (err) {
         console.log('ERROR POSTING EVENT :', err);
       }
@@ -118,7 +122,7 @@ function CreateEvent() {
                 },
               })}
             />
-            <MapSmall />
+            {/* <MapSmall /> */}
             {/* Rendering text based on participants added */}
             <div>
               {participantsToAdd.length > 0
@@ -146,11 +150,13 @@ function CreateEvent() {
               value="Create"
               style="fill"
             />
+            {/* Show the user a notification if there is one */}
             {(notification !== '')
           && <p>{notification}</p>}
           </form>
         </div>
       </div>
+      {/* Toggling the user list for participant selection */}
       <div>
         {showParticipants === true && (
         <UserList
