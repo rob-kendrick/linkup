ce56df0ddad95002ef5c1c5995a2097a6ff04f5c
Rafael Falk <rbrtrflflk@gmail.com>
2022-03-30 14:55:39 +0200

    feat(fe event detail): cancel, delete functionality

--- client/src/components/PopUp/PopUpBtn/PopUpBtn.tsx
+++ client/src/components/PopUp/PopUpBtn/PopUpBtn.tsx
@@ -5,37 +5,39 @@ type Props = {
   text: string;
   setShowPopup: Function;
   // eslint-disable-next-line react/require-default-props
-  onClick?: () => void;
+  onClick?: Function;
 }
 
 function PopUpBtn({ text, setShowPopup, onClick }: Props) {
   if (text === 'Linkup') {
     return (
-      <button className="pubtn__btn pubtn__btn--color" type="button" onClick={() => { setShowPopup(false); onClick!(); }}>
+      <button
+        className="pubtn__btn pubtn__btn--color"
+        type="button"
+        onClick={() => {
+          setShowPopup(false);
+        onClick!();
+        }}
+      >
         {' '}
         <p className="pubtn__txt">{text}</p>
         {' '}
       </button>
     );
   }
-
-  if (text === 'Yes, Cancel') {
-    return (
-      <button className="pubtn__btn" type="button" onClick={() => { setShowPopup(false); onClick!(); }}>
-        {' '}
-        <p className="pubtn__txt">{text}</p>
-        {' '}
-      </button>
-    );
-  }
-
   return (
-    <button className="pubtn__btn" type="button" onClick={() => { setShowPopup(false); }}>
+    <button
+      className="pubtn__btn"
+      type="button"
+      onClick={() => {
+        setShowPopup(false);
+      onClick!();
+      }}
+    >
       {' '}
       <p className="pubtn__txt">{text}</p>
       {' '}
     </button>
-
   );
 }
 
