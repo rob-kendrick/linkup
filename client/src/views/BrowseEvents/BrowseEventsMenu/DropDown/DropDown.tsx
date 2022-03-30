8fc15f632227543b16e7a967a8774a308541fece
Peter hp <phirschparker@gmail.com>
2022-03-30 14:20:08 +0200

    feat(browse-events-menu): styling changes and split into more modules

---
+++ client/src/views/BrowseEvents/BrowseEventsMenu/DropDown/DropDown.tsx
@@ -0,0 +1,58 @@
+// @ts-nocheck
+import React from 'react';
+import FaUsers from '../../../../assets/FaUsers.svg';
+import FaUser from '../../../../assets/FaUser.svg';
+import BiCalenderHeart from '../../../../assets/BiCalendarHeart.svg';
+import MdTitle from '../../../../assets/MdTitle.svg';
+import AiFillTag from '../../../../assets/AiFillTag.svg';
+import './DropDown.css';
+
+const dropDownData = [
+  {
+    text: 'Date',
+    svgLogo: BiCalenderHeart,
+  },
+  {
+    text: 'Title',
+    svgLogo: MdTitle,
+  },
+  {
+    text: 'Tags',
+    svgLogo: AiFillTag,
+  },
+  {
+    text: 'Host',
+    svgLogo: FaUser,
+  },
+  {
+    text: 'Participants',
+    svgLogo: FaUsers,
+  },
+
+];
+
+function DropDown({ handleSelectDropDown, currentFilter }:any) {
+  const dropDown = dropDownData.map((el) => (
+    <div className="dd__item-wrapper">
+      <div
+        className={`dd__item ${currentFilter === el.text ? 'dd__item-active' : ''}`}
+        onClick={() => handleSelectDropDown(el.text)}
+      >
+        <img alt={el.text} className="dd__item-icon" src={el.svgLogo} />
+        <p className="dd__item-text">{el.text}</p>
+
+      </div>
+      <hr className="dd_item-line" />
+    </div>
+
+  ));
+
+  return (
+    <div className="dd__container">
+      { dropDown }
+    </div>
+
+  );
+}
+
+export default DropDown;
