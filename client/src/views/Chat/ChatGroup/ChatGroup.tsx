15c04bdf728fcec376ffa616d010441d70b4f468
Rafael Falk <rbrtrflflk@gmail.com>
2022-03-31 11:56:10 +0200

    feat(fe chat): input field and send button

--- client/src/views/Chat/ChatGroup/ChatGroup.tsx
+++ client/src/views/Chat/ChatGroup/ChatGroup.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { io, Socket } from 'socket.io-client';
 import { useForm } from 'react-hook-form';
 import { InputTextArea } from '../../../components/Form/InputTextField/InputTextField';
 import HeaderReturn from '../../../components/HeaderReturn/HeaderReturn';
 import { LuEvent } from '../../../utilities/types/Event';
+import { ReactComponent as IoPaperPlane } from '../../../assets/IoPaperPlane.svg';
+
 import './chatGroup.css';
 
 interface LocationState {
@@ -22,6 +24,8 @@ interface ClientToServerEvents {
 }
 
 export default function ChatGroup() {
+  const [textareaheight, setTextareaheight] = useState(1);
+
   const userId = localStorage.getItem('id_user');
   const location = useLocation();
   const { state } = location as LocationState;
@@ -42,44 +46,54 @@ export default function ChatGroup() {
     createPost(userId, eventId, msg);
   });
 
-  const sendMessage = (input: any) => {
-    socket.emit('emitMsgFromClient', Number(userId), state.currentEvent.id_event, input.message);
-  };
-
   const {
     register,
+    reset,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
+  const sendMessage = (input: any) => {
+    setTextareaheight(1);
+    reset();
+    socket.emit('emitMsgFromClient', Number(userId), state.currentEvent.id_event, input.message);
+  };
+
+  function changeTextAreaHeight(event: any) {
+    const height = event.target.scrollHeight;
+    const rowHeight = 23;
+    const trows = Math.ceil(height / rowHeight) - 2;
+    if (textareaheight < 5) {
+      setTextareaheight(trows);
+    }
+  }
+
   return (
-    <article className="cG">
+    <div className="cG">
       <HeaderReturn text={state.currentEvent.title} luEvent={state.currentEvent} socket={socket} />
-      <div className="cG__mainContainer">
+      <div className="cG__main-container">
         <form onSubmit={handleSubmit(sendMessage)}>
-
-          <div id="msgArea" className="cG__chatMsgsContainer">Chat</div>
-
-          <div className="cG__inputContainer">
-            <div className="cG__input-field">
+          <div id="msgArea" className="cG__messages-container">Chat</div>
+          <div className="cG__input-container">
+            <div className="cG__input-textarea">
               <InputTextArea
                 type="text"
                 errorMessage={errors.description?.message}
-                rows={5}
+                rows={textareaheight}
+                id="itf__chat"
+                className="itf__chat-container"
                 {...register('message', {
                   required: 'This field is required',
-                  onChange: () => {
-                    console.log('hello');
-                  },
+                  onChange: (e) => changeTextAreaHeight(e),
                 })}
               />
             </div>
-            <div className="cG__button-container">
-              <button className="cG__button" type="submit">S</button>
-            </div>
+            <button className="cG__button" type="submit">
+              <IoPaperPlane className="cG__icon" />
+            </button>
           </div>
         </form>
       </div>
-    </article>
+    </div>
   );
 }
