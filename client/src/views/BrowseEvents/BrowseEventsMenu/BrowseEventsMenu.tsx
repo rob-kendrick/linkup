afe8a987a12c06befb6e44ac3801e9f1503c21ad
Peter hp <phirschparker@gmail.com>
2022-03-31 10:38:00 +0200

    refactor(browse events menu): added key to lists

--- client/src/views/BrowseEvents/BrowseEventsMenu/BrowseEventsMenu.tsx
+++ client/src/views/BrowseEvents/BrowseEventsMenu/BrowseEventsMenu.tsx
@@ -76,18 +76,20 @@ function BrowseEventsMenu({
 
   // render date list
   const dateList = datesNextMonth.map((el:any) => (
-    <>
+    <div
+      className="bem__fitlerbar-dates-item-container"
+      key={el.toString()}
+    >
       <button
         onClick={() => handleClickDate(el)}
-        key={el.toString()}
-        className={`bem__fitlerbar-dates-item ${dateSelected === el ? 'bem__selector-active' : ''}`}
+        className={`bem__fitlerbar-dates-item-btn ${dateSelected === el ? 'bem__selector-active' : ''}`}
         type="button"
       >
         <span className="bem__fitlerbar-dates-item-details">{moment(el).format('ddd')}</span>
         <span className="bem__fitlerbar-dates-item-details">{moment(el).format('DD')}</span>
       </button>
       <span className="bem__fitlerbar-dates-item-spacer" />
-    </>
+    </div>
   ));
 
   return (
